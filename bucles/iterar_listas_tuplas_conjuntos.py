695155cae4682d113a8e4e4bbe2e9c393b59f8e4
Jonathan <jonathancarranza70@gmail.com>
2023-08-11 10:14:54 -0300

    primer commit

--- bucles/iterar_listas_tuplas_conjuntos.py
+++ bucles/iterar_listas_tuplas_conjuntos.py
@@ -3,7 +3,6 @@ numeros = [10, 20, 55, 90]
 
 #recorriendo la lista animales
 for animal in animales :
-    animal = + f'a cada paso uno'
     print ( f'ahora la variable es igual a : {animal}')
 
 #recorriendo la lista num y multiplicando por 10
