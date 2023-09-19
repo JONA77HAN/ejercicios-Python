880a8339ca846002364f38df2b244be9953e5819
Jonathan <jonathancarranza70@gmail.com>
2023-09-18 23:10:32 -0300

    divinacion

--- clases_CFP/ejercicios_de_la_clase/1_blackjack_pc.py
+++ clases_CFP/ejercicios_de_la_clase/1_blackjack_pc.py
@@ -1,14 +1,13 @@
 from random import randint
-
 #machine = croupier 
 puntaje_pc = 0
 
 while puntaje_pc < 17 :
-    carta = randint(1,13)
-    if carta == 1 or carta == 11 or carta == 12 or carta == 13:
-        carta = 10
-    print( 'carta del croupier : ' + str(carta))    
-    puntaje_pc += carta
+    carta_pc = randint(1,13)
+    if carta_pc == 1 or carta_pc == 11 or carta_pc == 12 or carta_pc == 13:
+        carta_pc = 10
+    print( 'carta del croupier : ' + str(carta_pc))    
+    puntaje_pc += carta_pc
     print( ' Su puntaje ---> ' + str(puntaje_pc))
 if puntaje_pc > 21:
     print ('Croupier fuera de juego ')
