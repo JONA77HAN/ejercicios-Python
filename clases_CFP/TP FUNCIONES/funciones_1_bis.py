a1c634aaa0f49169ec02281a9e033ac3dbc86474
Jonathan <jonathancarranza70@gmail.com>
2023-09-22 11:34:39 -0300

    repitiendo los ejercicios de funciones

---
+++ clases_CFP/TP FUNCIONES/funciones_1_bis.py
@@ -0,0 +1,24 @@
+# Ejercicio5. 
+# Realizar una función que calcule: 
+# (muestre en pantalla) el área o el volumen de un cilindro, según se especifique. 
+# Para distinguir un caso de otro se le pasará el carácter 'a' (para área) o 'v' (para el volumen). 
+# Además, hemos de pasarle a la función el radio y la altura.
+# Volumen = Pi*Radio^2*altura
+# Area = 2*Pi*Radio*Altura
+
+def calcular(tipo, radio, altura):
+    if tipo == 'a':
+        area = round(2*3.14*altura, 2)
+        print(f'el area es de {area}')
+    elif:
+        volumen = round(3.14*radio**2*altura, 2)
+        print(f'el volumen es de {volumen}')
+    else:
+        print ('ingrese un tipo valido, "a" o "v"')    
+
+a = input('ingrese el tipo: ')
+b = int(input('ingrese el radio: '))
+c = int(input('ingrese la altura: '))
+
+calcular(a, b, c)
+
--- clases_CFP/TP FUNCIONES/funciones_8.py
+++ clases_CFP/TP FUNCIONES/funciones_8.py
@@ -2,15 +2,26 @@
 
 #http://recursostic.educacion.es/secundaria/edad/4esotecnologia/quincena5/imagenes5/pasar_decimal_binario.gif
 
-cadena = 0
-num = 15
-
-while num % 2:
-    if num == 0:
-        residuo = 0
+def mostrar_en_binario(numero):
+    if 0 <= numero <= 255:
+        if numero == 0:
+            print("El número 0 en binario es: 0")
+        else:
+            binario = ""
+            while numero > 0:
+                bit = numero % 2
+                binario = str(bit) + binario
+                numero //= 2
+            print(f"{numero} en binario es: {binario}")
     else:
-        num = 1
-cadena += residuo
+        print("El número está fuera del rango (0-255)")
+
+# Ejemplos de uso:
+mostrar_en_binario(42)
+mostrar_en_binario(128)
+mostrar_en_binario(255)
+mostrar_en_binario(0)
+mostrar_en_binario(300)
 
 
 
