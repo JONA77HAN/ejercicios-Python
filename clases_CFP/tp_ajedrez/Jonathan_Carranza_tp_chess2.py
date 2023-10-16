42dc8d737efdbcd25fb19f47038af4d97efe970d
Jonathan <jonathancarranza70@gmail.com>
2023-10-15 23:25:44 -0300

    iniciando tablero

--- clases_CFP/tp_ajedrez/Jonathan_Carranza_tp_chess2.py
+++ clases_CFP/tp_ajedrez/Jonathan_Carranza_tp_chess2.py
@@ -1,1 +1,20 @@
 from random import randint
+A8 = 0
+B8 = 0 
+C8 = 0
+D8 = 0
+E8 = 0
+F8 = 0
+G8 = 0
+H8 = 0
+print('     -------------------------------')
+print('8 -   '+ str(H8) +'   '+ str(G8) +'   '+ str(F8) +'   '+ str(E8) +'   '+ str(D8) +'   '+ str(C8) +'   '+ str(B8) +'   '+ str(A8))
+print('7 -   '+ str(H8) +'   '+ str(G8) +'   '+ str(F8) +'   '+ str(E8) +'   '+ str(D8) +'   '+ str(C8) +'   '+ str(B8) +'   '+ str(A8))
+print('6 -   '+ str(H8) +'   '+ str(G8) +'   '+ str(F8) +'   '+ str(E8) +'   '+ str(D8) +'   '+ str(C8) +'   '+ str(B8) +'   '+ str(A8))
+print('5 -   '+ str(H8) +'   '+ str(G8) +'   '+ str(F8) +'   '+ str(E8) +'   '+ str(D8) +'   '+ str(C8) +'   '+ str(B8) +'   '+ str(A8))
+print('4 -   '+ str(H8) +'   '+ str(G8) +'   '+ str(F8) +'   '+ str(E8) +'   '+ str(D8) +'   '+ str(C8) +'   '+ str(B8) +'   '+ str(A8))
+print('3 -   '+ str(H8) +'   '+ str(G8) +'   '+ str(F8) +'   '+ str(E8) +'   '+ str(D8) +'   '+ str(C8) +'   '+ str(B8) +'   '+ str(A8))
+print('2 -   '+ str(H8) +'   '+ str(G8) +'   '+ str(F8) +'   '+ str(E8) +'   '+ str(D8) +'   '+ str(C8) +'   '+ str(B8) +'   '+ str(A8))
+print('1 -   '+ str(H8) +'   '+ str(G8) +'   '+ str(F8) +'   '+ str(E8) +'   '+ str(D8) +'   '+ str(C8) +'   '+ str(B8) +'   '+ str(A8))
+print('     -------------------------------')
+print('      A   B   C   D   E   F   G   H')
