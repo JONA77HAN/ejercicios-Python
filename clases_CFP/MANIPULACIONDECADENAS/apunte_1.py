e5dd7815ba3aa2e320b5e37a05bd8b4d38ec77d4
Jonathan <jonathancarranza70@gmail.com>
2023-10-31 21:04:45 -0300

    continuar en un ratito

---
+++ clases_CFP/MANIPULACIONDECADENAS/apunte_1.py
@@ -0,0 +1,11 @@
+saludo = 'hola universo'
+print(saludo[0:4])
+print('hola' in 'hola mundo')
+print(saludo.upper())
+print('-'*50)
+print('Cómo estás?')
+meSiento = input()
+if meSiento.lower() == 'bien':
+ print('Yo me siento genial también!')
+else:
+ print('Espero que el resto de tu día mejore.')
