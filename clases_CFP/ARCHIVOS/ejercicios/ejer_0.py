b099e29c9a77f45b58203a98d2dd582755613949
Jonathan <jonathancarranza70@gmail.com>
2023-11-18 16:14:36 -0300

    sabado *2

--- clases_CFP/ARCHIVOS/ejercicios/ejer_0.py
+++ clases_CFP/ARCHIVOS/ejercicios/ejer_0.py
@@ -2,7 +2,7 @@
 # primero hago los cambios que quiero hacer
 directorio = 'clases_CFP/ARCHIVOS/ejercicios/almohada.txt'
 archivo = open(directorio, 'a')
-contenido = archivo.write('\ntodas las hojas son del viento\nYa que las mueve hasta en la muerte')
+contenido = archivo.write('')
 archivo.close()
 
 # ahora lo muestro
