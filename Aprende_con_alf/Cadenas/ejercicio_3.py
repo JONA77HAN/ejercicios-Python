4aeeeef4535571e2009727e889027456d64af8f3
Jonathan <jonathancarranza70@gmail.com>
2023-11-25 22:58:41 -0300

    aprende con alf ejercicio 4 de 'Cadenas'

---
+++ Aprende_con_alf/Cadenas/ejercicio_3.py
@@ -0,0 +1,12 @@
+# Ejercicio 3
+# Escribir un programa que pregunte el nombre del usuario en la consola y 
+# después de que el usuario lo introduzca. Muestre por pantalla <NOMBRE> 
+# tiene <n> letras, donde <NOMBRE> es el nombre de usuario en mayúsculas 
+# y <n> es el número de letras que tienen el nombre.
+
+nombre = input('Ingrese su nombre: ')
+
+print(f'Su nombre en Mayúsculas es ...\n{nombre.upper()}')
+print(f'y tiene {len(nombre)} letras')
+
+
