ae03f591379b9b472264d3d14d74c33821c8c60d
Jonathan <jonathancarranza70@gmail.com>
2023-12-08 22:15:57 -0300

    aprende con alf

---
+++ Aprende_con_alf/Listas y Tuplas/ejercicio_1.py
@@ -0,0 +1,12 @@
+# Ejercicio 1
+# Escribir un programa que almacene las asignaturas de un curso 
+# (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una 
+# lista y la muestre por pantalla.
+
+lista = []
+materia =''
+
+while materia != 'stop': 
+    materia = input('ingresa la materia: ')
+    lista.append(materia)
+    print(lista)
