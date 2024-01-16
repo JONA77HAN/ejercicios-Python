61a2691e718c270f68274c0f4b16245a030d0ce4
Jonathan <jonathancarranza70@gmail.com>
2024-01-15 22:38:15 -0300

    ijijiji

---
+++ Aprende_con_alf/Listas y Tuplas/bucles/bucle_while.py
@@ -0,0 +1,8 @@
+contador = 0
+
+#se va a ejecutar siempre q sea True, vuelta tras vuelta se verifica la condicion
+while contador < 16:
+   print (contador)
+   contador+=1
+print ('el while llego a su fin')     
+
