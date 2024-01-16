2c34269af6d281f914c7953724af8faa576b87ff
Jonathan <jonathancarranza70@gmail.com>
2024-01-15 22:39:13 -0300

    arreglando los cambios

--- bucles/bucle_while.py
+++
@@ -1,8 +0,0 @@
-contador = 0
-
-#se va a ejecutar siempre q sea True, vuelta tras vuelta se verifica la condicion
-while contador < 16:
-   print (contador)
-   contador+=1
-print ('el while llego a su fin')     
-
